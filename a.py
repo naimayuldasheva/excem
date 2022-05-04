5dcc67256e4b2d36823f10b42d43591c28d9c3fc
naimayuldasheva <naimayuldasheva@gmail.com>
2022-05-04 19:06:57 +0600

    first commit

---
+++ a.py
@@ -0,0 +1,33 @@
+# class Shool:
+
+#     def wear_uniform(self):
+#         return f'В школе все должны носить форму'
+
+# class University():
+#     def wear_uniform(self):
+#         return f'Можно носить любую одежду, если ты студент'
+
+# shool = Shool()
+# shool1 = University()
+# print(shool.wear_uniform())
+# print(shool1.wear_uniform())
+
+
+# class BankAccount:
+
+#     def __init__(self,balance):
+#         self.balance = balance
+#         self.balance = 0
+
+#     def deposeit(self,uvelich):
+#         self.balance = uvelich
+#         return self.balance
+
+#     def withdraw(self,umen):
+#         self.balance = umen
+#         return self.balance
+        
+# bank = BankAccount(1000)
+# print(bank.deposeit(1000))
+# print(bank.withdraw(100))
+    
